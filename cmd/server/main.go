8d613f7865fc2cf04aaf4670ab0d24b504360a0f
Daniel Martin <daniel.martin@rentokil-initial.com>
2022-03-25 11:02:56 +0000

    Connected to DB

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
 
-func Run()  {
+	"github.com/DanielMartin96/goddit/internal/db"
+)
+
+func Run() error {
 	fmt.Println("Running our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("failed to connect to database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("succesfully connected and pinged database")
+	return nil
 }
 
 func main()  {
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/DanielMartin96/goddit
 
 go 1.18
+
+require github.com/lib/pq v1.10.4
+
+require github.com/jmoiron/sqlx v1.3.4 // indirect
